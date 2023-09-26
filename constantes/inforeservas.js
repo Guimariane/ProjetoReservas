9497dc8c18b7a16fc0427de08239c64cc0f9c638
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-26 02:45:19 -0300

    Criação da Constante
    criação da constante de reservas

---
+++ constantes/inforeservas.js
@@ -0,0 +1,19 @@
+export const reservas = [
+    {
+        id: 1,
+        n_quarto: 2005,
+        cliente: "Rogério Ceni",
+        cpf: "00120052012",
+        data_entrada: '2023-09-17',
+        data_saida: '2023-09-24'
+    },
+    {
+        id: 2,
+        n_quarto: 2023,
+        cliente: "Dorival Junior",
+        cpf: "20230924917",
+        data_entrada: '2023-09-17',
+        data_saida: '2023-09-24'
+    },
+
+]
