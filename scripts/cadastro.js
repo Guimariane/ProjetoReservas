5ef2ec19194315695dac6855a65b65138b5d3db1
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-30 01:26:09 -0300

    Correção das Funções
    Correção das funções de cadastro onde boa parte se perdeu ao realizar o merge

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,14 +1,14 @@
-document.getElementById.add('cadastro-reserva').addEventListener('submit', cadastrarReserva)
+// Evento para o botão da página
+document.getElementById('cadastro-reserva').addEventListener('submit', cadastrarReserva)
 
+// Variáveis para serem usadas no fetch na função abaixo
 const num_quarto = document.getElementById('nquarto')
 const nome_cliente = document.getElementById('nome')
 const cpf = document.getElementById('cpf')
 const data_entrada = document.getElementById('dataentrada')
 const data_saida = document.getElementById('datasaida')
 
-
-
-
+// Função de cadastro da reserva
 function cadastrarReserva(event) {
     event.preventDefault()
 
@@ -21,7 +21,40 @@ function cadastrarReserva(event) {
     if(!numero_quarto || !n_cliente || !cpf_cliente || !data_inicio || !data_fim) {
         alert("Preencha os dados da reserva!")
     } else {
-        
-    }
+        fetch('http://localhost:3000/reservas', {
+            method: 'POST',
+            body: JSON.stringify({
+                numero_quarto: numero_quarto,
+                nome_cliente: n_cliente,
+                cpf: cpf_cliente,
+                data_entrada: data_inicio,
+                data_saida: data_fim
+            }),
+            headers: {
+                'Accept': 'applicantion/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (response.ok === false) {
+                    throw new Error()
+                }
+                return response.json()
+            })
+            .then(() => {
+                alert('Cadastrado com sucesso')
 
+                num_quarto.value = ""
+                nome_cliente.value = ""
+                cpf.value = ""
+                data_entrada.value = ""
+                data_saida.value = ""
+
+                window.location.href = "reservas.html"
+            })
+            .catch((error) => {
+                console.log(error)
+                alert("Falha ao tentar cadastrar a reserva")
+            })
+    }
 }
